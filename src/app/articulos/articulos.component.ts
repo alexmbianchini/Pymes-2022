e5ec5e6a2e6a06caf36962322cdecffbaee88e62
alexmbianchini <alexmaxbianchini@hotmail.com>
2022-06-05 14:38:14 -0300

    3 files modified

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -38,7 +38,46 @@ export class ArticulosComponent implements OnInit {
     { Id: false, Nombre: 'NO' },
   ];
 
+  submitted = false;
+
+  FormBusqueda = new FormGroup({
+    Nombre: new FormControl(null),
+    Activo: new FormControl(null),
+  });
+
+  FormRegistro = new FormGroup({
+    IdArticulo: new FormControl(0),
+    Nombre: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(55),
+    ]),
+    Precio: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{1,7}'),
+    ]),
+    Stock: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{1,7}'),
+    ]),
+    CodigoDeBarra: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{13}'),
+    ]),
+    IdArticuloFamilia: new FormControl('', [Validators.required]),
+    FechaAlta: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+      ),
+    ]),
+
+    Activo: new FormControl(true),
+  });
+
   constructor(
+    //private articulosService: MockArticulosService,
+    //private articulosFamiliasService: MockArticulosFamiliasService,
     private articulosService: ArticulosService,
     private articulosFamiliasService: ArticulosFamiliasService
   ) {}
@@ -103,6 +142,11 @@ export class ArticulosComponent implements OnInit {
 
   // grabar tanto altas como modificaciones
   Grabar() {
+    this.submitted = true;
+    // verificar que los validadores esten OK
+    if (this.FormRegistro.invalid) {
+      return;
+    }
     //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
     const itemCopy = { ...this.FormRegistro.value };
 
@@ -156,22 +200,6 @@ export class ArticulosComponent implements OnInit {
   ImprimirListado() {
     alert('Sin desarrollar...');
   }
-
-  FormBusqueda = new FormGroup({
-    Nombre: new FormControl(null),
-    Activo: new FormControl(null),
-  });
-
-  FormRegistro = new FormGroup({
-    IdArticulo: new FormControl(0),
-    Nombre: new FormControl(''),
-    Precio: new FormControl(null),
-    Stock: new FormControl(null),
-    CodigoDeBarra: new FormControl(''),
-    IdArticuloFamilia: new FormControl(''),
-    FechaAlta: new FormControl(''),
-    Activo: new FormControl(true),
-  });
 
   GetArticuloFamiliaNombre(Id: number) {
     var Nombre = this.Familias.find((x) => x.IdArticuloFamilia === Id)?.Nombre;
